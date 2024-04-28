8d749ff1577b0b0fc402bbec74265b9570924be1
DaiLoi2002 <loidai2002@gmail.com>
2024-04-29 01:00:47 +0700

    render user on press

--- modules/Chat/Chatbox.js
+++ modules/Chat/Chatbox.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TextInput, Button, FlatList, Text, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TextInput, Button, FlatList, Text, ScrollView, TouchableOpacity, Image, Dimensions } from 'react-native';
 import { FontAwesome, MaterialIcons, Ionicons } from '@expo/vector-icons'; // Import FontAwesome here
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const ChatBox = () => {
+const ChatBox = ({ route }) => {
+  const windowWidth = Dimensions.get('window').width;
+  const { item } = route.params;
   const [username, setUsername] = useState('');
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
@@ -11,16 +13,36 @@ const ChatBox = () => {
   
   return (
     <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <TouchableOpacity>
-          <Ionicons name="call" size={24} color="green" />
-        </TouchableOpacity>
-        <Text style={styles.header}>Chat</Text>
-        <TouchableOpacity>
-          <FontAwesome name="ellipsis-v" size={24} color="black" />
-        </TouchableOpacity>
-      </View>
+     <View style={styles.headerContainer}>
+  <Image
+    source={{ uri: item.avatar }}
+    style={{
+      marginLeft: windowWidth * 0.05, // Chiều rộng của ảnh là 10% của chiều rộng màn hình
+      width: windowWidth * 0.1, // Chiều rộng của ảnh là 20% của chiều rộng màn hình
+      height: windowWidth * 0.1, // Chiều cao của ảnh cũng là 20% của chiều rộng màn hình để tạo thành hình tròn
+      borderRadius: (windowWidth * 0.2) / 2, // Bán kính là nửa chiều rộng của ảnh
+    }} 
+  />
+  <Text style={[styles.header, { width: windowWidth * 0.6, marginLeft:windowWidth*0.1 }]}>{item.name}</Text> {/* Chiều rộng của văn bản là 60% của chiều rộng màn hình */}
+  <TouchableOpacity style={{ width: windowWidth * 0.1, alignItems: 'center' }}> {/* Chiều rộng của nút là 10% của chiều rộng màn hình */}
+    <Ionicons name="call" size={24} color="green" />
+  </TouchableOpacity>
+  <TouchableOpacity style={{ width: windowWidth * 0.1, alignItems: 'center' }}> {/* Chiều rộng của nút là 10% của chiều rộng màn hình */}
+    <FontAwesome name="ellipsis-v" size={24} color="black" />
+  </TouchableOpacity>
+</View>
       <View style={styles.chatContainer}>
+
+
+
+
+
+
+
+
+
+
+
         <FlatList
           data={messages}
           renderItem={({ item }) => (
@@ -37,30 +59,38 @@ const ChatBox = () => {
           contentContainerStyle={styles.messagesContainer}
           inverted
         />
+
+
+
+
+
+
+
+        
       </View>
       <View style={styles.inputContainer}>
-        <TouchableOpacity>
-          <MaterialIcons name="attach-file" size={24} color="black" />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <FontAwesome name="smile-o" size={24} color="black" /> {/* Change to smile-o */}
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <FontAwesome name="microphone" size={24} color="black" /> {/* Change to microphone */}
-        </TouchableOpacity>
-        <TextInput
-          style={styles.input}
-          value={inputMessage}
-          onChangeText={setInputMessage}
-          placeholder="Type your message here..."
-          placeholderTextColor="#777"
-          multiline={true}
-          numberOfLines={4}
-        />
-        <TouchableOpacity style={styles.sendButton}>
-        <Text style={styles.sendButtonText}>Send</Text>
-        </TouchableOpacity>
-      </View>
+  <TouchableOpacity style={{ width: '15%', alignItems: 'center' }}>
+    <MaterialIcons name="attach-file" size={24} color="black" />
+  </TouchableOpacity>
+  <TouchableOpacity style={{ width: '15%', alignItems: 'center' }}>
+    <FontAwesome name="smile-o" size={24} color="black" /> {/* Change to smile-o */}
+  </TouchableOpacity>
+ 
+  <TextInput
+    style={{ width: '45%', paddingHorizontal: 10, borderRadius:20 }} // Đặt chiều rộng là 50% và padding ngang là 10
+    value={inputMessage}
+    onChangeText={setInputMessage}
+    placeholder="Type your message here..."
+    placeholderTextColor="#777"
+    multiline={true}
+    numberOfLines={2}
+  />
+  <TouchableOpacity style={{ width: '15%', alignItems: 'center',marginLeft:"5%" }}>
+    <View style={styles.sendButton}>
+      <Text style={styles.sendButtonText}>Send</Text>
+    </View>
+  </TouchableOpacity>
+</View>
     </View>
   );
 };
@@ -125,6 +155,11 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold',
+  }, avatar: {
+    marginLeft:20,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 });
 
